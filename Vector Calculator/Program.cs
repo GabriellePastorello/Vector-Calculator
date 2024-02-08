de6b21a61312f9d02fb0cad1c114371ffa4f6acf
MUKILTEO\1717665 <1717665@mukilteo.wednet.edu>
2024-02-08 12:48:06 -0800

    Finished Calculator

--- Vector Calculator/Program.cs
+++ Vector Calculator/Program.cs
@@ -6,11 +6,322 @@ namespace Vector_Calculator
     {
         static void Main(string[] args)
         {
-            Vector test1 = new Vector(1, 2, 3);
-            Vector test2 = new Vector(-1, 2, -3);
-            
-            // This should be 0, 4, 0
-            Console.WriteLine(Vector.Add(test1, test2));
+            Vector test1 = new Vector(0, 0, 0);
+            Vector test2 = new Vector(0, 0, 0);
+
+            Console.WriteLine("Welcome to vector calculator.");
+            bool run = true;
+
+            while (run)
+            {
+                Console.WriteLine("\nChoose an option:");
+                Console.WriteLine("a) change vector 1 - " + test1);
+                Console.WriteLine("b) change vector 2 - " + test2);
+                Console.WriteLine("c) get the magnitude of a vector");
+                Console.WriteLine("d) get direction of a vector");
+                Console.WriteLine("e) add vectors");
+                Console.WriteLine("f) negate a vector");
+                Console.WriteLine("g) subtract vectors");
+                Console.WriteLine("h) scale a vector");
+                Console.WriteLine("i) normalize a vector");
+                Console.WriteLine("j) find the dot product of two vectors");
+                Console.WriteLine("k) find the cross product of two vectors");
+                Console.WriteLine("l) find the angle between two vectors");
+                Console.WriteLine("m) project a vector");
+                Console.WriteLine("n) exit vector calculator");
+                string input = Console.ReadLine();
+                if (input == "a")
+                {
+                    //change the values of vector 1
+                    while (input == "a")
+                    {
+                        Console.WriteLine("\nType an x value");
+                        input = Console.ReadLine();
+                        try
+                        {
+                            float.Parse(input);
+                        }
+                        catch (System.FormatException)
+                        {
+                            Console.WriteLine("Invalid Input.");
+                            input = "a";
+                        }
+                    }
+                    test1.x = float.Parse(input);
+                    input = "a";
+                    while (input == "a")
+                    {
+                        Console.WriteLine("\nType a y value");
+                        input = Console.ReadLine();
+                        try
+                        {
+                            float.Parse(input);
+                        }
+                        catch (System.FormatException)
+                        {
+                            Console.WriteLine("Invalid Input.");
+                            input = "a";
+                        }
+                    }
+                    test1.y = float.Parse(input);
+                    input = "a";
+                    while (input == "a")
+                    {
+                        Console.WriteLine("\nType a z value");
+                        input = Console.ReadLine();
+                        try
+                        {
+                            float.Parse(input);
+                        }
+                        catch (System.FormatException)
+                        {
+                            Console.WriteLine("Invalid Input.");
+                            input = "a";
+                        }
+                    }
+                    test1.z = float.Parse(input);
+                    Console.WriteLine("Here is your new vector: " + test1);
+                }
+                else if (input == "b")
+                {
+                    //change the values of vector 2
+                    while (input == "b")
+                    {
+                        Console.WriteLine("\nType an x value");
+                        input = Console.ReadLine();
+                        try
+                        {
+                            float.Parse(input);
+                        }
+                        catch (System.FormatException)
+                        {
+                            Console.WriteLine("Invalid Input.");
+                            input = "b";
+                        }
+                    }
+                    test2.x = float.Parse(input);
+                    input = "b";
+                    while (input == "b")
+                    {
+                        Console.WriteLine("\nType a y value");
+                        input = Console.ReadLine();
+                        try
+                        {
+                            float.Parse(input);
+                        }
+                        catch (System.FormatException)
+                        {
+                            Console.WriteLine("Invalid Input.");
+                            input = "b";
+                        }
+                    }
+                    test2.y = float.Parse(input);
+                    input = "b"; 
+                    while (input == "b")
+                    {
+                        Console.WriteLine("\nType a z value");
+                        input = Console.ReadLine();
+                        try
+                        {
+                            float.Parse(input);
+                        }
+                        catch (System.FormatException)
+                        {
+                            Console.WriteLine("Invalid Input.");
+                            input = "b";
+                        }
+                    }
+                    test2.z = float.Parse(input);
+                    Console.WriteLine("Here is your new vector: " + test2);
+                }
+                else if (input == "c")
+                {
+                    //will calculate the magnitude of a vector
+                    while ( input == "c")
+                    {
+                        Console.WriteLine("\nWhich vector do you want to get the magnitude of?");
+                        Console.WriteLine("v1) " + test1);
+                        Console.WriteLine("v2) " + test2);
+                        input = Console.ReadLine();
+                        if (input == "v1")
+                        {
+                            Console.WriteLine("The magnitude of v1 is: " + test1.GetMagnitude());
+                        }
+                        else if (input == "v2")
+                        {
+                            Console.WriteLine("The magnitude of v2 is: " + test2.GetMagnitude());
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid Input.");
+                            input = "c";
+                        }
+                    }
+                    
+                }
+                else if (input == "d")
+                {
+                    //gets the direction of a vector
+                    while (input == "d")
+                    {
+                        Console.WriteLine("\nWhich vector do you want to get the direction of?");
+                        Console.WriteLine("v1) " + test1);
+                        Console.WriteLine("v2) " + test2);
+                        input = Console.ReadLine();
+                        if (input == "v1")
+                        {
+                            Console.WriteLine("The direction of " + test1 + " is: " + test1.GetDirection() + " deg");
+                        }
+                        else if (input == "v2")
+                        {
+                            Console.WriteLine("The direction of " + test2 + " is: " + test2.GetDirection() + " deg");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid Input.");
+                            input = "d";
+                        }
+                    }
+                }
+                else if (input == "e")
+                {
+                    //adds two vectors
+                    Console.WriteLine(test1 + " + " + test2 + " = " + Vector.Add(test1, test2));
+                }
+                else if (input == "f")
+                {
+                    //negates a vector
+                    while (input == "f")
+                    {
+                        Console.WriteLine("\nWhich vector would you like to negate?");
+                        Console.WriteLine("v1) " + test1);
+                        Console.WriteLine("v2) " + test2);
+                        input = Console.ReadLine();
+                        if (input == "v1")
+                        {
+                            Console.WriteLine("v1 negated is: " + Vector.Negate(test1));
+                        }
+                        else if (input == "v2")
+                        {
+                            Console.WriteLine("v2 negated is: " + Vector.Negate(test2));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid Input.");
+                            input = "f";
+                        }
+                    }
+                    
+                }
+                else if (input == "g")
+                {
+                    //subtracts vector 1 by vector 2
+                    Console.WriteLine(test1 + " - " + test2 + " = " + Vector.Subtract(test1, test2));
+                }
+                else if (input == "h")
+                {
+                    //scales a vector
+                    while (input == "h")
+                    {
+                        Console.WriteLine("\nWhich vector do you want to scale?");
+                        Console.WriteLine("v1) " + test1);
+                        Console.WriteLine("v2)" + test2);
+                        input = Console.ReadLine();
+                        if (input == "v1")
+                        {
+                            Console.WriteLine("What would you like to scale v1 by?");
+                            input = Console.ReadLine();
+                            Console.WriteLine(test1 + " scaled by " + input + ": " + Vector.Scale(test1, float.Parse(input)));
+                        }
+                        else if (input == "v2")
+                        {
+                            Console.WriteLine("What would you like to scale v2 by?");
+                            input = Console.ReadLine();
+                            Console.WriteLine(test2 + " scaled by " + input + ": " + Vector.Scale(test2, float.Parse(input)));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid Input.");
+                            input = "h";
+                        }
+                    }
+                    
+                }
+                else if (input == "i")
+                {
+                    //normalizes a vector
+                    while (input == "i")
+                    {
+                        Console.WriteLine("\nWhich vector would you like to normalize?");
+                        Console.WriteLine("v1) " + test1);
+                        Console.WriteLine("v2) " + test2);
+                        input = Console.ReadLine();
+                        if (input == "v1")
+                        {
+                            Console.WriteLine(test1 + " normalized is: " + Vector.Normalize(test1));
+                        }
+                        else if (input == "v2")
+                        {
+                            Console.WriteLine(test2 + " normalized is: " + Vector.Normalize(test2));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid Input.");
+                            input = "i";
+                        }
+                    }
+                }
+                else if (input == "j")
+                {
+                    //finds dot product of vectors
+                    Console.WriteLine("\nThe dot product of " + test1 + " and " + test2 + " is: " + Vector.DotProduct(test1, test2));
+                }
+                else if (input == "k")
+                {
+                    //finds cross product of vectors
+                    Console.WriteLine("\nThe cross product of " + test1 + " and " + test2 + " is: " + Vector.CrossProduct(test1, test2));
+                }
+                else if (input == "l")
+                {
+                    //finds angle between two vectors
+                    Console.WriteLine("\nThe angle between " + test1 + " and " + test2 + " is: " + Vector.AngleBetween(test1, test2) + " rad");
+
+                }
+                else if (input == "m")
+                {
+                    // vector projection
+                    while (input == "m")
+                    {
+                        Console.WriteLine("\nWhich vector would you like to project on the other?");
+                        Console.WriteLine("v1) " + test1 + " onto " + test2);
+                        Console.WriteLine("v2) " + test2 + " onto " + test1);
+                        input = Console.ReadLine();
+                        if (input == "v1")
+                        {
+                            Console.WriteLine("\n" + test1 + " projected onto " + test2 + " is: " + Vector.ProjectOnto(test1, test2));
+                        }
+                        else if (input == "v2")
+                        {
+                            Console.WriteLine("\n" + test2 + "projected onto " + test1 + " is: " + Vector.ProjectOnto(test2, test1));
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nInvalid Input.");
+                        }
+                    }
+                }
+                else if (input == "n")
+                {
+                    //exit
+                    Console.WriteLine("\nGoodbye.");
+                    run = false;
+                }
+                else
+                {
+                    //if an invalid input is given
+                    Console.WriteLine("\nInvalid input. Try again.");
+                }
+            }
         }
     }
 }
--- Vector Calculator/Vector.cs
+++ Vector Calculator/Vector.cs
@@ -25,68 +25,90 @@ namespace Vector_Calculator
 
         public float GetMagnitude()
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            // square root of x^2 + y^2
+            float squareX = x * x;
+            float squareY = y * y;
+            float squareZ = z * z;
+            //gets x squared, y squared and z squared
+            float mag = squareX + squareY + squareZ;
+            mag = MathF.Sqrt(mag);
+            //adds the x, y and z and square roots them
+            return mag;
         }
 
         public float GetDirection()
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            // theta = atan (y/x)
+            return MathF.Atan(y / x);
         }
 
         public static Vector Add(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return new Vector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+            //adds the x,y, and z values of two vectors
         }
 
         public static Vector Negate(Vector v)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return new Vector(v.x * -1, v.y * -1, v.z * -1);
+            //turns each value of the vector into negative if the value is positive, or positive if the value is negative
         }
 
         public static Vector Subtract(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return new Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+            //subtracts the x, y, and z values of two vectors
         }
 
-        public static Vector Scale(Vector v)
+        public static Vector Scale(Vector v, float s)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            //scales vector by s by multiplying x, y and z by s
+            return new Vector(v.x * s, v.y * s, v.z * s);
         }
 
         public static Vector Normalize(Vector v)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            // divide x, y and z by the magnitude
+            float magnitude = v.GetMagnitude();
+            return new Vector(v.x / magnitude, v.y / magnitude, v.z / magnitude);
         }
 
         public static float DotProduct(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            // dot product = (AX * BX) + (AY * BY) + (AZ * BZ)
+            float xProduct = v1.x * v2.x;
+            float yProduct = v1.y * v2.y;
+            float zProduct = v1.z * v2.z;
+            return xProduct + yProduct + zProduct;
         }
 
         public static Vector CrossProduct(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            // Cx = EyAz - EzAy
+            //Cy = EzAx - ExAz
+            //Cz = ExAy - EyAx
+            float Cx = (v1.y * v2.z) - (v1.z * v2.y);
+            float Cy = (v1.z * v2.x) - (v1.x * v2.z);
+            float Cz = (v1.x * v2.y) - (v1.y * v2.x);
+            return new Vector(Cx, Cy, Cz);
         }
 
-        public static Vector AngleBetween(Vector v1, Vector v2)
+        public static float AngleBetween(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            // Theta = acos (dotProduct/magnitudeA * magnitudeB)
+            float numerator = DotProduct(v1, v2);
+            float denominator = v1.GetMagnitude() * v2.GetMagnitude();
+            return MathF.Acos(numerator / denominator);
         }
 
         public static Vector ProjectOnto(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            // proj (V) U = dot product/ (magnitude of v) squared
+            float dotProd = Vector.DotProduct(v1, v2);
+            float magnitude = v2.GetMagnitude();
+            magnitude = MathF.Pow(magnitude, 2);
+            float scalar = dotProd / magnitude;
+            return Vector.Scale(v2, scalar);
         }
     }
 }
